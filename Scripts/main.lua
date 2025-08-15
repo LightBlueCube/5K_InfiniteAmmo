9c485347082bffa5e05993e7f2bc66ed7b9489c0
LightBlueCube <115393812+LightBlueCube@users.noreply.github.com>
2025-08-15 23:04:10 +0800

    make it more stable

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -1,7 +1,6 @@
 print("[InfiniteAmmo] Loaded!")
 
-local AMMO_REGAIN_AMOUNT = 10	-- Count, Add how many of ammo for each TICK_PER_TIME
-local TICK_PER_TIME = 100		-- Millisecond, Usually doesnt need to change
+local TIME_PER_TICK = 1000		-- Millisecond, How many of milliseconds we do a check and modify, usually doesnt need to change
 
 local PlayerCharacterTracker = {}
 
@@ -11,30 +10,25 @@ ExecuteWithDelay(2000, function()
 		if not FPSCharacterObj:IsPlayerControlled() then
 			return
 		end
-	
+
 		PlayerCharacterTracker[FPSCharacterObj.PlayerState.PlayerID] = FPSCharacterObj
 	end)
 end)
 
-LoopAsync(TICK_PER_TIME, function()
+LoopAsync(TIME_PER_TICK, function()
 	ExecuteInGameThread(function()
 		for ID, FPSCharacter in pairs(PlayerCharacterTracker) do
 			if not FPSCharacter:IsValid() then
 				PlayerCharacterTracker[ID] = nil
 			end
 
-			local AFPSItem = FPSCharacter.EquippedItem
-			if not AFPSItem:IsValid() then
+			local FPSRangedWeapon = FPSCharacter.EquippedItem
+			if not FPSRangedWeapon:IsValid() then
 				goto continue
 			end
 
-			if not AFPSItem:CanAddAmmo(AMMO_REGAIN_AMOUNT) then
-				goto continue
-			end
-
-			AFPSItem:AddAmmo(false, AMMO_REGAIN_AMOUNT)
-			AFPSItem:ServerCheckAmmo()
-			AFPSItem.Delay = 0.0
+			FPSRangedWeapon.Magazines = {32767,32767}
+			FPSRangedWeapon.CurrentAmmo = 32767
 
 			::continue::
 		end
